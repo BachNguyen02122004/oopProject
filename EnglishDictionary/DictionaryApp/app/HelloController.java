ea42ffb853b4b53883f53877a5aa0262ed5b2df6
BachNguyen02122004 <bachtramtinh04@gmail.com>
2023-10-23 12:45:33 +0700

    update form

---
+++ EnglishDictionary/DictionaryApp/app/HelloController.java
@@ -0,0 +1,15 @@
+package DictionaryApp.app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Xin chào các bạn");
+    }
+
+}
